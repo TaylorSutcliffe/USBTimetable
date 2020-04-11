f6793b4bac357bb39afab8b30f8556c218cf7152
Taylor <t.sutcliffe@ncl.ac.uk>
2020-04-11 19:07:40 +0100

    added a timetable api function using i calendar

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import requests 
 import json
-
+from icalendar import Calendar, Event
+import datetime
 
 values = ["occupancy-sensor", "co2", "room-temperature", "relative-humidity"]
 
@@ -23,7 +24,34 @@ def apiGet(room, sensor, tfStart = None, tfEnd = None):
         data = returned["latest"]
 
 
-    print(data)
+    #print(data)
     return returned
+
+def timeTableGet(studentNo):
+    g = open("calendar.ics", "w")
+    #print(requests.get(f"https://m.ncl.ac.uk/itservice/ical/ical.php?personal={studentNo}&type=C;F;H;L;P;S;T;V;W;A").text)
+    g.write(requests.get(f"https://m.ncl.ac.uk/itservice/ical/ical.php?personal={studentNo}&type=C;F;H;L;P;S;T;V;W;A").text)
+    g.close()
+    g = open("calendar.ics", "r")
+    gcal = Calendar.from_ical(g.read())
+    dic = {}
+    l = []
+    for component in gcal.walk():
+         if component.name == "VEVENT":
+            dic["title"] = component.decoded('SUMMARY')
+            dic["start"] = component.decoded('dtstart').strftime("%Y-%M-%dT%XZ")
+            dic["end"] = component.decoded('dtend').strftime("%Y-%M-%dT%XZ")
+            dic["location"] = component.decoded('Location')
+            l.append(dict(dic))
+            #print(component.decoded('SUMMARY'))
+            #print(component.decoded('dtstart'))
+            #print(component.decoded('dtend'))
+            #print(component.decoded('Location'))
+    g.close()
+
+    return l
+
+
     
-apiGet("room-6.025", values[0], tfStart="2019-04-27T00:00:00Z", tfEnd="2019-05-27T23:59:59")
+#apiGet("room-6.025", values[0], tfStart="2019-04-27T00:00:00Z", tfEnd="2019-05-27T23:59:59")
+#print(timeTableGet(160508552))
