3c56b68e9b60ea1f017bc7dda571d824206478a1
Taylor <t.sutcliffe@ncl.ac.uk>
2020-04-12 15:16:14 +0100

    fixed api function for rooms with multiple zones

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ from icalendar import Calendar, Event
 import datetime
 
 values = ["occupancy-sensor", "co2", "room-temperature", "relative-humidity"]
+roomId = {}
 
 
 def apiGet(room, sensor, tfStart = None, tfEnd = None):
@@ -16,16 +17,40 @@ def apiGet(room, sensor, tfStart = None, tfEnd = None):
         tfStart, tfEnd: tfEnd must be greater than tfStart and both in format 2019-04-27T00:00:00Z"""
     returned = None
     data = None
-    if tfStart and tfEnd:
-        returned = requests.get(f"https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/timeseries/{room}/{sensor}/raw/historic?startTime={tfStart}&endTime={tfEnd}").json()
-        data = returned["historic"]["values"]
+    if room == "room-4.022" or "room-2.022": #add rooms here with multiple zones
+        if sensor == "occupancy-sensor":
+            room = room.replace(".", "-")    
+            if tfStart and tfEnd:
+                returned = requests.get(f"https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/timeseries/{room}-zone-3/{sensor}/raw/historic?startTime={tfStart}&endTime={tfEnd}").json()
+            else:
+                returned = requests.get(f"https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/timeseries/{room}-zone-3/{sensor}/raw/").json()
+                data = returned["latest"]
+        else:
+            room = room.replace("room-", "")
+            if tfStart and tfEnd:
+                if room in roomId:
+                    returned = requests.get(f'https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/timeseries/{roomId[room]}/historic?startTime={tfStart}&endTime={tfEnd}').json()
+                else:
+                    print(f"https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity?meta:roomNumber={room}&metric={sensor}")
+                    tid = requests.get(f'https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity?meta:roomNumber={room}&metric={sensor}').json()
+                    timeseriesid= tid['items'][0]['feed'][0]['timeseries'][0]['timeseriesId']
+                    roomId[room] = timeseriesid
+                    returned = requests.get(f'https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/timeseries/{timeseriesid}/historic?startTime={tfStart}&endTime={tfEnd}').json()
+                data = returned["historic"]["values"]
+            else:
+                returned = requests.get(f"https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/timeseries/{room}-zone-3/{sensor}/raw/").json()
+                data = returned["latest"]
     else:
-        returned = requests.get(f"https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/timeseries/{room}/{sensor}/raw/").json()
-        data = returned["latest"]
+        if tfStart and tfEnd:
+            returned = requests.get(f"https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/timeseries/{room}/{sensor}/raw/historic?startTime={tfStart}&endTime={tfEnd}").json()
+            data = returned["historic"]["values"]
+        else:
+            returned = requests.get(f"https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/timeseries/{room}/{sensor}/raw/").json()
+            data = returned["latest"]
 
 
     #print(data)
-    return returned
+    return data
 
 def timeTableGet(studentNo):
     g = open("calendar.ics", "w")
@@ -38,10 +63,10 @@ def timeTableGet(studentNo):
     l = []
     for component in gcal.walk():
          if component.name == "VEVENT":
-            dic["title"] = component.decoded('SUMMARY')
-            dic["start"] = component.decoded('dtstart').strftime("%Y-%M-%dT%XZ")
-            dic["end"] = component.decoded('dtend').strftime("%Y-%M-%dT%XZ")
-            dic["location"] = component.decoded('Location')
+            dic["title"] = component.decoded('SUMMARY').decode("utf-8")
+            dic["start"] = component.decoded('dtstart').strftime("%Y-%m-%dT%XZ")
+            dic["end"] = component.decoded('dtend').strftime("%Y-%m-%dT%XZ")
+            dic["location"] = component.decoded('Location').decode("utf-8")
             l.append(dict(dic))
             #print(component.decoded('SUMMARY'))
             #print(component.decoded('dtstart'))
@@ -53,5 +78,5 @@ def timeTableGet(studentNo):
 
 
     
-#apiGet("room-6.025", values[0], tfStart="2019-04-27T00:00:00Z", tfEnd="2019-05-27T23:59:59")
+#print(apiGet("room-6.025", values[0], tfStart="2019-04-27T00:00:00Z", tfEnd="2019-05-27T23:59:59Z"))
 #print(timeTableGet(160508552))
